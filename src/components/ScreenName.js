d9ea08e81d28cd937218bd33e5cdc6e4592176b2
heinerbehrends <heinerbehrends@hotmail.com>
2018-08-06 08:23:32 +0200

    added autoFocus

--- src/components/ScreenName.js
+++ src/components/ScreenName.js
@@ -25,7 +25,7 @@ class ScreenName extends Component {
         <form id="screenName" name="screenName" className="form-inline" onSubmit={this.sendScreenName}>
           <label className="sr-only" htmlFor="enter-screen-name">Screen name</label>
           <div className="form-group">
-            <input onChange={this.handleChange} className="form-control border-right-0 rounded-0" type="text" size="35" id="enter-screen-name" name="screenName" placeholder="Enter your screen name to play"/>
+            <input onChange={this.handleChange} className="form-control border-right-0 rounded-0" type="text" size="35" id="enter-screen-name" name="screenName" autoFocus />
             <input className="form-control rounded-0" type="submit" value="Submit" />
           </div>
         </form>
