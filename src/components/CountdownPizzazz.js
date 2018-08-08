1fe41b1233169f8ad8a7616c4f2d25b3c2286c53
heinerbehrends <heinerbehrends@hotmail.com>
2018-08-08 23:16:25 +0200

    Added circle to countdown. No animation yet.

--- src/components/CountdownPizzazz.js
+++ src/components/CountdownPizzazz.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import { endGameAction } from '../actions'
 
 const renderer = ({ seconds }) => {
-  return <span className = "text-secondary display-1 pt-4 d-inline-block">{ seconds }</span>;
+  return <span style={{left: 45 + 'px', bottom: 45 + 'px'}} className = "text-secondary display-1 pt-4 position-absolute">{ seconds }</span>;
 }
 
 class CountdownPizzazz extends Component {
@@ -15,17 +15,20 @@ class CountdownPizzazz extends Component {
 
   render() {
     let { countdown, makeMove } = this.props;
-    console.log(countdown);
     if (countdown !== true) {
-      return null;
+      return null
     }
     else {
-      console.log(makeMove);
       return (
+        <div className = "position-relative d-inline" style = {{ width: 160 + 'px', height: 160 + 'px', lineHeight: 200 + 'px'}}>
           <Countdown  date = {Date.now() + 20000}
           renderer = { renderer }
           onComplete = { () => (this.props.endGame(this.props.firstPlayer, this.props.makeMove)) }
           />
+          <svg width="200" height="200">
+            <circle r="90" cx="100" cy="100" fill="none" stroke="darkgray" strokeWidth="6"></circle>
+          </svg>
+        </div>
       )
     }
   }
