97279676cb2c6b7e9cb864dda3e47f09c7e9e40e
heinerbehrends <heinerbehrends@hotmail.com>
2018-09-10 00:28:28 +0200

    Added logic that prevents the tiles to be moved when the countdown is not active

--- src/components/ValidWordHTML.js
+++ src/components/ValidWordHTML.js
@@ -2,32 +2,12 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import ScrabbleBoard from './styled/ValidWordHTMLStyled'
 import DraggableTile from './DraggableTile'
-import { letterValues } from '../Constants'
-import { replaceLetter } from '../scrabbleLogic/gameLogic'
-import { replaceLetterAction, showValidAction } from '../actions/gameActions'
-
+import makeTilesArray from '../scrabbleLogic/makeTilesArray'
 
 class ValidWordHTML extends Component {
   render() {
-    const tilesArray = [];
-    let i = 0;
-    for (let randomLetter of this.props.validWord) {
-      tilesArray.push(
-        <DraggableTile
-          letter={ randomLetter }
-          letterValues={ letterValues }
-          string = { this.props.validWord  }
-          showValid = { this.props.showValid > i }
-          index={ i }
-          key={ i }
-          parent="validWord"
-          replaceLetter={ replaceLetter }
-          replaceLetterAction={ replaceLetterAction }
-          showValidAction={ showValidAction }
-        />
-      )
-      i++;
-    }
+    const { validWord, showValid, isCountdown } = this.props;
+    const tilesArray = makeTilesArray(isCountdown, validWord, showValid);
     return (
       <ScrabbleBoard>
         { tilesArray }
@@ -37,9 +17,11 @@ class ValidWordHTML extends Component {
 }
 
 const mapStateToProps = state => {
-  return { validWord: state.validWord,
-           showValid: state.showValid,
-           potentialScore: state.potentialScore }
+  return {
+    validWord: state.validWord,
+    showValid: state.showValid,
+    isCountdown: state.isCountdown,
+         }
 }
 
 export default connect(mapStateToProps)(ValidWordHTML);
---
+++ src/scrabbleLogic/makeTilesArray.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { letterValues } from '../Constants'
+import { replaceLetter } from '../scrabbleLogic/gameLogic'
+import { replaceLetterAction, showValidAction } from '../actions/gameActions'
+import ScrabbleTile from '../components/ScrabbleTile'
+import DraggableTile from '../components/DraggableTile'
+
+function makeTilesArray(isCountdown, validWord, showValid) {
+
+  const tilesArray = [];
+  let i = 0;
+
+  for (let letter of validWord) {
+
+    if (isCountdown) {
+      tilesArray.push(
+        <DraggableTile
+          letter={ letter }
+          letterValues={ letterValues }
+          string = { validWord  }
+          showValid = { showValid > i }
+          index={ i }
+          key={ i }
+          parent="validWord"
+          replaceLetter={ replaceLetter }
+          replaceLetterAction={ replaceLetterAction }
+          showValidAction={ showValidAction }
+        />
+      )
+      i++;
+    }
+
+    else {
+      tilesArray.push(
+        <ScrabbleTile
+          letter={ letter }
+          index={ i }
+          letterValues= { letterValues }
+          isValid = { false }
+        />
+      )
+    }
+  }
+
+  return tilesArray
+}
+
+export default makeTilesArray
