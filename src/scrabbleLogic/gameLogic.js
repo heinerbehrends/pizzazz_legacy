f37ea1f47f46967eab857ac06a84ea33721c528e
heinerbehrends <heinerbehrends@hotmail.com>
2018-09-12 11:39:08 +0200

    Wrote more readable function updateString, removed useless, added getScore

--- src/scrabbleLogic/gameLogic.js
+++ src/scrabbleLogic/gameLogic.js
@@ -1,27 +1,49 @@
-export function replaceLetter(string, letter, index) {
-  let newString = string.substring(0, index)
-                    .concat(letter)
-                    .concat(string.substring(index + 1));
-  return newString;
-}
 
-export function updateValidWord(props, target) {
-  const { letter, string, index, parent, replaceLetter } = props;
+export const updateString = (props, target, randomOrValid) => {
+
+  const { letter, string, index, parent } = props;
   const { targetLetter, targetIndex, targetParent, targetString } = target;
+  const isParent = (parent === randomOrValid);
+  const isTargetParent = (targetParent === randomOrValid);
 
-  if (targetParent === 'validWord') {
-    if (parent === 'validWord') {
-      let firstMutation = replaceLetter(targetString, letter, targetIndex);
-      let secondMutation = replaceLetter(firstMutation, targetLetter, index);
-      return secondMutation;
-    }
-    else {
-      return replaceLetter(targetString, letter, targetIndex);
-    }
+  if (isParent && isTargetParent) {
+    return swapLetters(props, target)
+  }
+  else if (isParent && !isTargetParent) {
+    return replaceLetter(string, targetLetter, index)
+  }
+  else if (!isParent && isTargetParent) {
+    return replaceLetter(targetString, letter, targetIndex);
   }
   else {
-    if (parent === 'validWord') {
-      return replaceLetter(string, targetLetter, index)
-    }
+    return false;
   }
+}
+
+
+export const replaceLetter = (string, letter, index) => {
+  return string.substring(0, index)
+                    .concat(letter)
+                    .concat(string.substring(index + 1));
+}
+
+const swapLetters = (props, target) => {
+  let firstMutation = replaceLetter(target.targetString, props.letter, target.targetIndex);
+  return replaceLetter(firstMutation, target.targetLetter, props.index);
+}
+
+
+export const getScore = (word, letterValues) => {
+  var score = 0;
+  let index = 0;
+  for (let letter of word) {
+    if (index === 4) {
+      score += letterValues[letter] * 2;
+    }
+    else {
+      score += letterValues[letter];
+    }
+    index++;
+  }
+  return score;
 }
