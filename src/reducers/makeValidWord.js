2a501225b061591596b998307840bdbf1e7ecdd7
heinerbehrends <heinerbehrends@hotmail.com>
2018-09-12 11:35:16 +0200

    got rid off logic in reducer

--- src/reducers/makeValidWord.js
+++ src/reducers/makeValidWord.js
@@ -3,14 +3,7 @@ import { REPLACE_LETTER, MAKE_MOVE } from '../actionTypes'
 const validWordReducer = (state = "0000000", action) => {
   switch(action.type) {
     case REPLACE_LETTER:
-      if (action.target === 'validWord') {
-        return state.substring(0, action.index)
-                    .concat(action.letter)
-                    .concat(state.substring(action.index + 1));
-      }
-      else {
-        return state;
-      }
+      return action.validWord ? action.validWord : state
     case MAKE_MOVE:
       let zeros = '';
       for (let i = 0; i < action.index; i++) {
