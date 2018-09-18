270bbb5dd0cf64ed4b3d2429dd06b65b7b53bdf4
heinerbehrends <heinerbehrends@hotmail.com>
2018-09-18 20:19:29 +0200

    Refactored Countdown components

--- src/reducers/isCountdown.js
+++ src/reducers/isCountdown.js
@@ -1,10 +1,10 @@
-import { START_GAME, STOP_COUNTDOWN } from '../actionTypes'
+import { START_GAME, END_GAME } from '../actionTypes'
 
 const countdownReducer = (state = false, action) => {
   switch (action.type) {
     case START_GAME:
       return true;
-    case STOP_COUNTDOWN:
+    case END_GAME:
       return false;
     default:
       return state;
