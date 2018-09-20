c917556f2f4d366a92cb5b9d0eb143111f381db5
heinerbehrends <heinerbehrends@hotmail.com>
2018-09-21 00:14:35 +0200

    Changed default HTML5 drag preview to empty picture

--- src/components/DraggableTile.js
+++ src/components/DraggableTile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { DragSource, DropTarget } from 'react-dnd'
+import { getEmptyImage } from 'react-dnd-html5-backend'
 import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { letterSource, letterTarget } from './DraggableSourceTarget'
@@ -9,14 +10,27 @@ import ScrabbleTile from './ScrabbleTile'
 
 class DraggableTile extends Component {
 
+  componentDidMount() {
+		const { connectDragPreview } = this.props
+		if (connectDragPreview) {
+			// Use empty image as a drag preview so browsers don't draw it
+			// and we can draw whatever we want on the custom drag layer instead.
+			connectDragPreview(getEmptyImage(), {
+				// IE fallback: specify that we'd rather screenshot the node
+				// when it already knows it's being dragged so we can hide it with CSS.
+				captureDraggingState: true,
+			})
+    }
+  }
+
   render() {
-    const { letter, index, connectDragSource, connectDropTarget, showValid } = this.props;
+    const { letter, index, connectDragSource, connectDropTarget, showValid, isDragging } = this.props;
 
     return connectDragSource(
               connectDropTarget(
                 <div style={{ width: 13.68 + "%" }}>
                   <ScrabbleTile
-                    letter={ letter }
+                    letter={ isDragging ? '0' : letter }
                     index={ index }
                     isValid = { showValid }
                   />
@@ -30,6 +44,7 @@ DraggableTile.propTypes = {
   letter: PropTypes.string.isRequired,
   index: PropTypes.number.isRequired,
   showValid: PropTypes.bool.isRequired,
+  isDragging: PropTypes.bool,
   validWords: PropTypes.arrayOf(PropTypes.string).isRequired,
   connectDragSource: PropTypes.func.isRequired,
   connectDropTarget: PropTypes.func.isRequired,
@@ -39,8 +54,10 @@ const mapStateToProps = state => ({
   validWords: state.gameData.validWords,
 })
 
-const dragCollect = connect => ({
-  connectDragSource: connect.dragSource()
+const dragCollect = (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  connectDragPreview: connect.dragPreview(),
+  isDragging: monitor.isDragging(),
 })
 
 const dropCollect = connect => ({
