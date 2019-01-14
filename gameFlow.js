3f7fc4c118cb6a6616bd4e7b99596a8d69d10ca8
heinerbehrends <heinerbehrends@hotmail.com>
2019-01-14 14:59:38 +0100

    Encapsulated server state in a closure with a getter and a setter.

--- gameFlow.js
+++ gameFlow.js
@@ -3,33 +3,41 @@ import findAllValidWords from './serverLogic/findValidWords';
 
 const duration = 50;
 
-let state = {
+const initialState = {
   randomLetters: '',
   validWords: [],
   seconds: duration,
 };
 
-export const getState = () => state;
+const useState = (initial) => {
+  let state = initial;
+  const getState = () => state;
+  const setState = (newState) => { state = newState; };
+  return Object.freeze({ getState, setState });
+};
+
+export const { getState, setState } = useState(initialState);
 
 export function* gameFlow(io) {
   while (true) {
     const randomLetters = makeRandomLetters(7);
     const validWords = findAllValidWords(randomLetters);
-    state = {
-      ...state,
+    setState({
+      ...initialState,
       randomLetters,
       validWords,
-    };
-    yield io.emit('StartGame', state);
+    });
+    yield io.emit('StartGame', getState());
 
     while (true) {
-      yield state = { ...state, seconds: state.seconds - 1 };
-      const { seconds } = state;
+      const state = getState();
+      yield setState({ ...state, seconds: state.seconds - 1 });
+      const { seconds } = getState();
       if (seconds === 10) {
         io.emit('EndGame', { type: 'END_GAME' });
       }
       if (seconds === 0) {
-        state = { ...state, seconds: duration };
+        setState({ ...state, seconds: duration });
         break;
       }
     }
