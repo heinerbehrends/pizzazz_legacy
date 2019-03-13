4ead71a2946ac91478df19b033f93b6e21194f8d
heinerbehrends <heinerbehrends@hotmail.com>
2019-03-13 21:41:14 +0100

    Refactored ScreenName component using the useState hook

--- client/src/Inputs/ScreenName/ScreenName.js
+++ client/src/Inputs/ScreenName/ScreenName.js
@@ -1,46 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   FormContainer, Form, FormGroup, HiddenLabel, ButtonForm, TextInput,
-} from '../ScreenName/ScreenNameStyled';
+} from './ScreenNameStyled';
 
-export default class ScreenName extends Component {
-  constructor(...args) {
-    super(...args);
-    this.state = { screenName: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.sendScreenName = this.sendScreenName.bind(this);
-  }
+const ScreenName = (props) => {
+  const [input, setInput] = useState('');
+  const { sendName, screenName } = props;
 
-  handleChange({ target: { value } }) {
-    this.setState({ screenName: value });
-  }
+  const handleChange = ({ target: { value } }) => setInput(value);
 
-  sendScreenName(event) {
+  const sendScreenName = (event) => {
     event.preventDefault();
-    const { sendName } = this.props;
-    const { screenName } = this.state;
-    if (!screenName) {
+    if (!input) {
       return;
     }
-    sendName(screenName);
-  }
+    sendName(input);
+  };
 
-  render() {
-    const { screenName } = this.props;
-    return screenName.length ? null : (
+  return (
+    screenName.length ? null : (
       <FormContainer>
         <Form
           id="screenName"
           name="screenName"
           autoComplete="off"
-          onSubmit={this.sendScreenName}
+          onSubmit={sendScreenName}
         >
           <HiddenLabel htmlFor="enter-screen-name">Enter screen name</HiddenLabel>
           <FormGroup>
             <TextInput
               placeholder="Enter screen name to start"
-              onChange={this.handleChange}
+              onChange={handleChange}
               type="text"
               id="enter-screen-name"
               name="screenName"
@@ -50,11 +41,13 @@ export default class ScreenName extends Component {
           </FormGroup>
         </Form>
       </FormContainer>
-    );
-  }
-}
+    )
+  );
+};
 
 ScreenName.propTypes = {
   sendName: PropTypes.func.isRequired,
   screenName: PropTypes.string.isRequired,
 };
+
+export default ScreenName;
