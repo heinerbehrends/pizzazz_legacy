5b4720e00caee9195e3c6f2a697f2f03d5d73e48
heinerbehrends <heinerbehrends@hotmail.com>
2019-03-14 23:23:21 +0100

    changed path of build folder in server index.js, Updated snapshots

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/build/public/index.html'));
 });
